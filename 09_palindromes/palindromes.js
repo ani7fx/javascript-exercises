10872612c27b7b5f8173eac4f008cdcc627120b5
Anirudh Madhavan <madhavaa@tcd.ie>
2024-08-25 10:03:27 +0200

    did 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    // let arr = str.split("");
+    let arr = str
+        .toLowerCase()
+        .split("")
+        .filter(char => alphanumerical.includes(char))
+        .join("");
+
+    let reverse = arr.split("").reverse().join("");
+    return (arr == reverse);
 };
 
 // Do not edit below this line
