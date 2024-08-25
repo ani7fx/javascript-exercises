b19fc6d18af215321852bab20ca0ea69d49534fd
Anirudh Madhavan <madhavaa@tcd.ie>
2024-08-25 10:17:14 +0200

    did 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (typeof position != 'number'){
+        position = parseInt(position)
+    }
+    if (position == 0) return 0
+    if (position < 0) return "OOPS"
+    let firstPrev = 1;
+    let SecondPrev = 0;
+    for (i = 2; i <= position; i++){
+        let current = firstPrev + SecondPrev;
+        SecondPrev = firstPrev
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
